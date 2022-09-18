d87703429291819f81537a2543be10d53a69a43d
Sam Davey <samkdavey@gmail.com>
2022-09-18 16:53:15 +0100

    Unique slug image loading for products & layout integrated

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>
-      
+    <div className="layout">
+      <Head>
+        <title>Sam Davey Store</title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className="main-container">
+        { children }
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   )
 }
